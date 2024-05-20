dd86f9ca50161ba9f344ee85d6a5d8d855985bab
matej-kovar <matej.kovar.023@pslib.cz>
2024-05-20 13:27:31 +0200

    save

--- main.ts
+++ main.ts
@@ -77,7 +77,7 @@ function display(gridData: TileData[], dim: Size): void {
             gridData[i].tileTypeOptions = tileSet;
     }
     }
-    function weightedRandom(weights:number[], generatedNum:number) {
+    /*function weightedRandom(weights:number[], generatedNum:number) {
         let cumulativeWeights:number[] = [];
         for (let i = 0; i < weights.length; i += 1) {
           cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
@@ -94,6 +94,7 @@ function display(gridData: TileData[], dim: Size): void {
         }
       }
     }
+    */
 function generateDungeonLevelRooms(gridData: TileData[], dim: Size) {
     const random = splitmix32((globalSeed) >>> 0) 
     for (let index: number = 0; index < gridData.length; index++) {
